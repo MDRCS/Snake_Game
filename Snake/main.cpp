fbabee8632b40f933b6b9f2d28bc39701ec2ed27
MDRCS <elrahali.md@CS.com>
2018-04-21 22:26:30 +0100

    Initial Commit

---
+++ Snake/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Snake
+//
+//  Created by Mohamed El Rahali on 4/21/18.
+//  Copyright © 2018 Mohamed El Rahali. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
